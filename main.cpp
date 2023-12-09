e2425375c0ee1511f3f05829b0dda4237b351792
Alexey Karcev <116674575+krawwwwy@users.noreply.github.com>
2023-12-09 15:23:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 
 int main() {
-    int a = 1, b = 1, c = 35;
-    std::vector<double> result = solve(a, b, c);
-    std::cout << "Roots: " << result[0] << " and " << result[1] << std::endl;
+    int a = 1, b = 4, c = -7;
+    std::vector<std::string> result = solve(a, b, c);
+    if (result.size() == 2) {
+       std::cout << "Roots: " << result[0] << " and " << result[1] << std::endl;
+    }
+    else {
+        std::cout << "Only one root: " << result[0] << std::endl;
+    }
     return 0;
 }
-  
